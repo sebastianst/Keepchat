a07e1980f318f0aedd5aa4cdd1278a723075881a
Sebastian Stammler <stammler.s@gmail.com>
2013-08-12 16:20:28 +0200

    added OnSharedPreferencesChanged Listener
    Now the summary text under the ListPreference items will always reflect the current selection.

--- src/com/sturmen/xposed/keepchat/SettingsActivity.java
+++ src/com/sturmen/xposed/keepchat/SettingsActivity.java
@@ -1,9 +1,5 @@
 package com.sturmen.xposed.keepchat;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.preference.PreferenceFragment;
-
 /**
  * Copyright (C) 2013 Nick Tinsley (00sturm@gmail.com), Sebastian Stammler (stammler@cantab.net)
  * Created on 8/9/13.
@@ -23,20 +19,58 @@ import android.preference.PreferenceFragment;
  * You should have received a copy of the GNU General Public License
  * a gazillion times.  If not, see <http://www.gnu.org/licenses/>.
  */
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.PreferenceFragment;
+
 public class SettingsActivity extends Activity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getFragmentManager().beginTransaction().replace(android.R.id.content, new PrefFragment()).commit();
+        if (savedInstanceState == null)
+            getFragmentManager().beginTransaction().replace(android.R.id.content, new PrefFragment()).commit();
     }
-    public static class PrefFragment extends PreferenceFragment {
+
+    public static class PrefFragment extends PreferenceFragment
+            implements SharedPreferences.OnSharedPreferenceChangeListener {
 
         @Override
         public void onCreate(final Bundle savedInstanceState)
         {
             super.onCreate(savedInstanceState);
+
+            getPreferenceManager().setSharedPreferencesMode(MODE_WORLD_READABLE);
             addPreferencesFromResource(R.xml.preferences);
+            updateListSummary("pref_imageSaving");
+            updateListSummary("pref_videoSaving");
+        }
+
+        @Override
+        public void onResume() {
+            super.onResume();
+            getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+        }
+
+        @Override
+        public void onPause() {
+            super.onPause();
+            getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+        }
+
+        public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+            if (key.equals("pref_imageSaving") || key.equals("pref_videoSaving")) {
+                updateListSummary(key);
+            }
+        }
+
+        private void updateListSummary(String key) {
+            ListPreference savePref = (ListPreference) findPreference(key);
+            // Set summary to be the user-description for the selected value
+            savePref.setSummary(savePref.getEntry());
         }
     }
 }
